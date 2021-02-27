79ec1a1dfb508bfe713f0f2393505690bcddedfd
VladAverin <Vladaverin@users.noreply.github.com>
2021-02-27 07:38:44 +0300

    init: decision

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
+module.exports = function towelSort(matrix) {
 
-// You should implement your task here.
+  if (matrix == undefined) {
 
-module.exports = function towelSort (matrix) {
-  return [];
+    return []
+
+  } else {
+    var ma1 = matrix.slice(3, 4).flat().reverse()
+    var ma2 = matrix.slice(0, 1).flat()
+    var ma3 = matrix.slice(1, 2).flat().reverse()
+    var ma4 = matrix.slice(2, 3).flat()
+
+    var result = ma2.concat(ma3, ma4, ma1)
+
+    return result
+  }
+
 }
